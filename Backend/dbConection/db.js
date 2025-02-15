263aee644085d38c4471a403e203b66676861167
Prince Olusegun Ojugbele <ojugbeleolusegun@gmail.com>
2025-02-14 20:38:34 -0800

    db updated

--- Backend/dbConection/db.js
+++ Backend/dbConection/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'Prince@1981',
-    host: 'localhost',
-    port: 5432,
-    database: 'perntodo'
+   
 });
 
 
